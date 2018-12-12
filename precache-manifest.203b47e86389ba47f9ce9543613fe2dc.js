cfa4c8204466e1b2ab9c1e197a99ece7023edc6c
ΣΤΡ (ΕΠ) Κατσούρης Δημήτριος <dkatsouris@army.gr>
2018-12-12 19:57:36 +0200

    Updates

---
+++ precache-manifest.203b47e86389ba47f9ce9543613fe2dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "e4f707eb8257d40b61ad",
+    "url": "/burger/static/js/runtime~main.e4f707eb.js"
+  },
+  {
+    "revision": "ee5c82aba38b413e8712",
+    "url": "/burger/static/js/main.ee5c82ab.chunk.js"
+  },
+  {
+    "revision": "802e7366b72ed1da6e40",
+    "url": "/burger/static/js/1.802e7366.chunk.js"
+  },
+  {
+    "revision": "ee5c82aba38b413e8712",
+    "url": "/burger/static/css/main.dcf18750.chunk.css"
+  },
+  {
+    "revision": "a8c6f1a67c34f51205087f09acd73ca7",
+    "url": "/burger/index.html"
+  }
+];
